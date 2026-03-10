15ab99b12b5824aff232fbfd8b7a510775f4fa0b
hev0s <niels.delafontaine@eduvaud.ch>
2026-03-10 10:24:36 +0100

    Fix:Suggetsion and navigation error

--- routes/navigation.js
+++ routes/navigation.js
@@ -16,32 +16,42 @@ const verifyToken = (req, res, next) => {
     });
 };
 
+// Post avec Photon
 router.post('/geocode', verifyToken, async (req, res) => {
-    const { address } = req.body;
+    const { address, lat, lon } = req.body;
 
     if (!address) return res.status(400).json({ error: 'Adresse manquante' });
 
     try {
-        const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`, {
-            method: 'GET',
-            headers: { 'User-Agent': 'PreTpi-Navigation-App/1.0 (Projet_TPI_CPNV)' }
-        });
+        let apiUrl = `https://photon.komoot.io/api/?q=${encodeURIComponent(address)}&limit=1`;
+
+        if (lat && lon) {
+            apiUrl += `&lat=${lat}&lon=${lon}`;
+        } else {
+            apiUrl += `&lat=46.8&lon=6.5`;
+        }
+
+        const response = await fetch(apiUrl);
 
         if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
 
         const data = await response.json();
 
-        if (data && data.length > 0) {
-            res.status(200).json({ lat: data[0].lat, lon: data[0].lon });
+        // Photon renvoie les données au format GeoJSON
+        if (data.features && data.features.length > 0) {
+            //Gestion de GeoJSON
+            const coords = data.features[0].geometry.coordinates;
+            res.status(200).json({ lat: coords[1], lon: coords[0] });
         } else {
             res.status(404).json({ error: 'Adresse introuvable' });
         }
     } catch (error) {
+        console.error("Erreur de géocodage:", error);
         res.status(500).json({ error: 'Erreur lors du calcul' });
     }
 });
 
-// Priorisation position GPS
+// API Photon avec priorité GPS (suggestion)
 router.post('/autocomplete', verifyToken, async (req, res) => {
     const { text, lat, lon } = req.body;
     if (!text) return res.status(400).json({ error: 'Texte manquant' });
@@ -49,11 +59,9 @@ router.post('/autocomplete', verifyToken, async (req, res) => {
     try {
         let apiUrl = `https://photon.komoot.io/api/?q=${encodeURIComponent(text)}&limit=5`;
 
-        // utilisation de la position GPS si disponible
         if (lat && lon) {
             apiUrl += `&lat=${lat}&lon=${lon}`;
         } else {
-            // Sinon, position par défaut (Centre de la Suisse Romande)
             apiUrl += `&lat=46.8&lon=6.5`;
         }
 
