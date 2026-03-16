05869537ef1348a3aa239e9455a9314b874b0e9e
hev0s <niels.delafontaine@eduvaud.ch>
2026-03-16 15:25:59 +0100

    Updated LinkWithDatabase to add incidents or remove them

--- Database/LinkWithDatabase.js
+++ Database/LinkWithDatabase.js
@@ -34,22 +34,23 @@ export async function deleteUser(userId) {
     }
 }
 
-export async function setFavoritePlace(userId, PlaceName, placeId) {
+export async function setFavoritePlace(userId, placeName, address, latitude, longitude) {
     try {
         const [result] = await db.query(
-            'INSERT INTO saved_locations (user_id, label, address) VALUES (?,?,?)',
-            [userId, PlaceName, placeId]
+            'INSERT INTO saved_locations (user_id, label, address, latitude, longitude) VALUES (?,?,?,?,?)',
+            [userId, placeName, address, latitude, longitude]
         );
         return result.insertId;
     } catch (err) {
         console.error('Insert error:', err.message);
+        throw err;
     }
 }
 
 export async function getFavoritePlaces(userId) {
     try {
         const [rows] = await db.query(
-            'SELECT id, label, address FROM saved_locations WHERE user_id = ?',
+            'SELECT id, label, address, latitude, longitude FROM saved_locations WHERE user_id = ?',
             [userId]
         );
         return rows;
@@ -67,10 +68,38 @@ export async function deleteFavoritePlace(userId, placeId) {
     }
 }
 
-export async function postIncident(userId, typeId) {
+export async function postIncident(userId, typeId, latitude, longitude, description = null) {
     try {
-        await db.query('INSERT INTO incidents (user_id, type_id) VALUES (?,?)', [userId, typeId]);
+        const [result] = await db.query(
+            'INSERT INTO incidents (user_id, type_id, latitude, longitude, description) VALUES (?,?,?,?,?)',
+            [userId, typeId, latitude, longitude, description]
+        );
+        return result.insertId;
     } catch (err) {
-        console.error('Insert error:', err.message);
+        console.error('Insert incident error:', err.message);
+        throw err;
+    }
+}
+
+export async function getActiveIncidents() {
+    try {
+        const [rows] = await db.query(
+            `SELECT i.id, i.latitude, i.longitude, i.description, t.label as type, i.created_at
+             FROM incidents i
+                      JOIN incident_types t ON i.type_id = t.id
+             WHERE i.is_active = TRUE`
+        );
+        return rows;
+    } catch (err) {
+        console.error('Select incidents error:', err.message);
+        throw err;
+    }
+}
+
+export async function deactivateIncident(incidentId) {
+    try {
+        await db.query('UPDATE incidents SET is_active = FALSE WHERE id = ?', [incidentId]);
+    } catch (err) {
+        console.error('Deactivate incident error:', err.message);
     }
 }
