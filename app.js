f094e891fff37ab8dfcee4fd1238186c5e8e1b23
hev0s <niels.delafontaine@eduvaud.ch>
2026-03-18 16:24:59 +0100

    Try dictionnary

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(cors({
 // Le dossier des images
 app.use('/images', express.static(path.join(__dirname, 'Docs/Images/Edited')));
 
+// Le dossier des scripts frontend (dictionnaire langues)
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+
 // HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
